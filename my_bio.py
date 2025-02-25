0f2ae7f0f053788cfce3c0c1eab5a953b4905ebc
Vitor Pritsch <123944801+vitor-pritsch@users.noreply.github.com>
2025-02-25 14:48:03 -0300

    Add files via upload

---
+++ my_bio.py
@@ -0,0 +1,18 @@
+# Had an idea to create a different bio for myself;
+class Developer:
+    def __init__(self):
+        self.entry_python = True
+        self.entry_java = False
+        print("--------------Python Developer--------------")
+        
+    def solve_challenge(self):
+        print("Programming changed the way I see the world!")
+        print("Positive Vibes + Clean Coding == Commit")
+
+dev = Developer()
+dev.solve_challenge()
+
+print("Open and ready for the next challenge!")
+print("_________________|o|_________________")
+
+# developed by Vitor Prit$ch
